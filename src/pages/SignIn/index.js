11028760d8f2fb36d1cfd514aea56f5f921fcd89
Sidney Santos <sidney_ssantos@hotmail.com>
2019-07-28 20:03:45 -0300

    Modulo 09 - Utilizando Unform

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <form>
-        <input type="email" placeholder="Insira seu e-mail" />
-        <input type="password" placeholder="Insira sua senha" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Insira seu e-mail" />
+        <Input name="password" type="password" placeholder="Insira sua senha" />
 
         <button type="submit">Acessar</button>
         <Link to="/register">Criar conta gratuita</Link>
-      </form>
+      </Form>
     </>
   );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <form>
-        <input type="text" placeholder="Nome completo" />
-        <input type="email" placeholder="Insira seu e-mail" />
-        <input type="password" placeholder="Insira sua senha" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="nome" type="text" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Insira seu e-mail" />
+        <Input name="password" type="password" placeholder="Insira sua senha" />
 
         <button type="submit">Criar conta</button>
         <Link to="/">Já tenho login</Link>
-      </form>
+      </Form>
     </>
   );
 }
