159f0aafb534759ba65eef58cef57134b7c1c17b
Sidney Santos <sidney_ssantos@hotmail.com>
2019-07-31 22:02:13 -0300

    Modulo 09 - Autenticaçao

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -5,12 +5,14 @@ import { Route, Redirect } from 'react-router-dom';
 import AuthLayout from '~/pages/_layouts/auth';
 import DefaultLayout from '~/pages/_layouts/default';
 
+import store from '~/store';
+
 export default function RouteWrapper({
   component: Component,
   isPrivate,
   ...rest
 }) {
-  const signed = false;
+  const { signed } = store.getState().auth;
 
   // caso não esteja logado e a rota é privada
   // redireciona para a tela de login
