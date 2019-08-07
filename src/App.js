c01ee9acc80fdddbd39e89570d938ab8d777dcce
Sidney Santos <sidney_ssantos@hotmail.com>
2019-08-06 22:11:02 -0300

    Modulo 09 - Persistindo autenticaçao

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
@@ -9,17 +10,19 @@ import history from './services/history';
 
 // obrigatório vir depois da importação do Reactotron
 // senão não terá acesso as configurações do sagaMonitor ou do createEnhancer
-import store from './store';
+import { store, persistor } from './store';
 
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Routes />
-        <GlobalStyle />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyle />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
