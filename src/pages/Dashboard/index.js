48bdcdccc87a9d07913857dd69161442994018d5
Sidney Santos <sidney_ssantos@hotmail.com>
2019-08-08 22:06:48 -0300

    Modulo 09 - Requisiçoes autenticada

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import api from '~/services/api';
 
 export default function Dashboard() {
+  api.get('appointments');
+
   return <h1>Dashboard</h1>;
 }
--- src/pages/_layouts/default/styles.js
+++ src/pages/_layouts/default/styles.js
@@ -2,5 +2,6 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   height: 100%;
+  min-height: 100vh;
   background: #333;
 `;
