0085b28554fcd11aee8426ecd29041025e3dc9f7
Sidney Santos <sidney_ssantos@hotmail.com>
2019-08-12 21:22:49 -0300

    Modulo 09 - Logout da Aplicaçao

--- src/pages/_layouts/auth/styles.js
+++ src/pages/_layouts/auth/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { darken } from 'polished';
 
 export const Wrapper = styled.div`
-  height: 100%;
+  height: 100vh;
   background: linear-gradient(-90deg, #715c91, #ab59c1);
   display: flex;
   align-items: center;
