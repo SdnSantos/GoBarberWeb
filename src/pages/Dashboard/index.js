dc017e940963042e2f3bd8d88dc0aada69568e33
Sidney Santos <sidney_ssantos@hotmail.com>
2019-08-12 22:40:19 -0300

    Modulo 09 - Estilizaçao do Dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,41 @@
 import React from 'react';
 import api from '~/services/api';
 
-export default function Dashboard() {
-  api.get('appointments');
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
-  return <h1>Dashboard</h1>;
+import { Container, Time } from './styles';
+
+export default function Dashboard() {
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>31 de Maio</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Diego Fernandes</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Em aberto</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Diego Fernandes</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Diego Fernandes</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
